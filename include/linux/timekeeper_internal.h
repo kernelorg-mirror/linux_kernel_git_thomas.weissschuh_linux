376d8443676b2cc32f175312bb27e12f0e4bdb37
Anna-Maria Behnsen <anna-maria@linutronix.de>
2024-05-23 17:02:35 +0200

    time: Introduce auxiliary POSIX clocks
    To support auxiliary timekeeping and the related user space interfaces,
    it's required to define a clock ID range for them.
    
    Reserve 8 auxiliary clock IDs after the regular timekeeping clock ID space.
    
    This is the maximum number of auxiliary clocks the kernel can support. The actual
    number of supported clocks depends obviously on the presence of related devices
    and might be constraint by the available VDSO space.
    
    Add the corresponding timekeeper IDs as well.
    
    Signed-off-by: Anna-Maria Behnsen <anna-maria@linutronix.de>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/linux/timekeeper_internal.h
+++ include/linux/timekeeper_internal.h
@@ -14,10 +14,16 @@
 /**
  * timekeeper_ids - IDs for various time keepers in the kernel
  * @TIMEKEEPER_CORE:	The central core timekeeper managing system time
+ * @TIMEKEEPER_AUX:	The first AUX timekeeper
+ * @TIMEKEEPER_AUX_LAST:The last AUX timekeeper
  * @TIMEKEEPERS_MAX:	The maximum number of timekeepers managed
  */
 enum timekeeper_ids {
 	TIMEKEEPER_CORE,
+#ifdef CONFIG_POSIX_AUX_CLOCKS
+	TIMEKEEPER_AUX,
+	TIMEKEEPER_AUX_LAST = TIMEKEEPER_AUX + MAX_AUX_CLOCKS - 1,
+#endif
 	TIMEKEEPERS_MAX,
 };
 
