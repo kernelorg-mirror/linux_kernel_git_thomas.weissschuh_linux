ae8b9e4a4927962de67600d42d6507f48f34cea2
Thomas Weißschuh <linux@weissschuh.net>
2024-08-18 11:19:40 +0200

    drm/amd/display: Remove spurious declaration of dm_helpers_get_sbios_edid()
    The prototype is the whole content of commit
    575d0df6dae4 ("drm/amd/display: refine the EDID override").
    Apparently the definition was never added.
    
    Fixes: 575d0df6dae4 ("drm/amd/display: refine the EDID override")
    Signed-off-by: Thomas Weißschuh <linux@weissschuh.net>

--- drivers/gpu/drm/amd/display/dc/dm_helpers.h
+++ drivers/gpu/drm/amd/display/dc/dm_helpers.h
@@ -208,8 +208,6 @@ int dm_helpers_dmub_set_config_sync(struct dc_context *ctx,
 		enum set_config_status *operation_result);
 enum adaptive_sync_type dm_get_adaptive_sync_support_type(struct dc_link *link);
 
-enum dc_edid_status dm_helpers_get_sbios_edid(struct dc_link *link, struct dc_edid *edid);
-
 bool dm_helpers_is_fullscreen(struct dc_context *ctx, struct dc_stream_state *stream);
 bool dm_helpers_is_hdr_on(struct dc_context *ctx, struct dc_stream_state *stream);
 
