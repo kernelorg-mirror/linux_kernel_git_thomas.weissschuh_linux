fd58112d99c10e9a572ffba604a9999442dab802
Thomas Weißschuh <linux@weissschuh.net>
2024-11-30 23:13:47 +0100

    gb audio prep

--- drivers/staging/greybus/audio_manager_module.c
+++ drivers/staging/greybus/audio_manager_module.c
@@ -21,10 +21,10 @@ static inline struct gb_audio_manager_module *to_gb_audio_module(struct kobject 
 struct gb_audio_manager_module_attribute {
 	struct attribute attr;
 	ssize_t (*show)(struct gb_audio_manager_module *module,
-			struct gb_audio_manager_module_attribute *attr,
+			const struct gb_audio_manager_module_attribute *attr,
 			char *buf);
 	ssize_t (*store)(struct gb_audio_manager_module *module,
-			 struct gb_audio_manager_module_attribute *attr,
+			 const struct gb_audio_manager_module_attribute *attr,
 			 const char *buf, size_t count);
 };
 
@@ -74,7 +74,7 @@ static void gb_audio_module_release(struct kobject *kobj)
 }
 
 static ssize_t gb_audio_module_name_show(struct gb_audio_manager_module *module,
-					 struct gb_audio_manager_module_attribute *attr, char *buf)
+					 const struct gb_audio_manager_module_attribute *attr, char *buf)
 {
 	return sprintf(buf, "%s", module->desc.name);
 }
@@ -83,7 +83,7 @@ static struct gb_audio_manager_module_attribute gb_audio_module_name_attribute =
 	__ATTR(name, 0664, gb_audio_module_name_show, NULL);
 
 static ssize_t gb_audio_module_vid_show(struct gb_audio_manager_module *module,
-					struct gb_audio_manager_module_attribute *attr, char *buf)
+					const struct gb_audio_manager_module_attribute *attr, char *buf)
 {
 	return sprintf(buf, "%d", module->desc.vid);
 }
@@ -92,7 +92,7 @@ static struct gb_audio_manager_module_attribute gb_audio_module_vid_attribute =
 	__ATTR(vid, 0664, gb_audio_module_vid_show, NULL);
 
 static ssize_t gb_audio_module_pid_show(struct gb_audio_manager_module *module,
-					struct gb_audio_manager_module_attribute *attr, char *buf)
+					const struct gb_audio_manager_module_attribute *attr, char *buf)
 {
 	return sprintf(buf, "%d", module->desc.pid);
 }
@@ -101,7 +101,7 @@ static struct gb_audio_manager_module_attribute gb_audio_module_pid_attribute =
 	__ATTR(pid, 0664, gb_audio_module_pid_show, NULL);
 
 static ssize_t gb_audio_module_intf_id_show(struct gb_audio_manager_module *module,
-					    struct gb_audio_manager_module_attribute *attr,
+					    const struct gb_audio_manager_module_attribute *attr,
 					    char *buf)
 {
 	return sprintf(buf, "%d", module->desc.intf_id);
@@ -112,7 +112,7 @@ static struct gb_audio_manager_module_attribute
 	__ATTR(intf_id, 0664, gb_audio_module_intf_id_show, NULL);
 
 static ssize_t gb_audio_module_ip_devices_show(struct gb_audio_manager_module *module,
-					       struct gb_audio_manager_module_attribute *attr,
+					       const struct gb_audio_manager_module_attribute *attr,
 					       char *buf)
 {
 	return sprintf(buf, "0x%X", module->desc.ip_devices);
@@ -123,7 +123,7 @@ static struct gb_audio_manager_module_attribute
 	__ATTR(ip_devices, 0664, gb_audio_module_ip_devices_show, NULL);
 
 static ssize_t gb_audio_module_op_devices_show(struct gb_audio_manager_module *module,
-					       struct gb_audio_manager_module_attribute *attr,
+					       const struct gb_audio_manager_module_attribute *attr,
 					       char *buf)
 {
 	return sprintf(buf, "0x%X", module->desc.op_devices);
