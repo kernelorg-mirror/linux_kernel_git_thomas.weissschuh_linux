88056236bb35cceed7c4ebd8571a537fd28a71dd
Thomas Gleixner <tglx@linutronix.de>
2024-12-05 15:51:42 +0100

    timekeeping: Add AUX offset to struct timekeeper
    This offset will be used in the time getters of auxiliary clocks. It is
    added to the "monotonic" clock readout.
    
    As auxiliary clocks do not utilize the offset fields of the core time
    keeper, this is just an alias for offs_tai, so that the cache line layout
    stays the same.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/linux/timekeeper_internal.h
+++ include/linux/timekeeper_internal.h
@@ -67,6 +67,7 @@ struct tk_read_base {
  * @offs_real:			Offset clock monotonic -> clock realtime
  * @offs_boot:			Offset clock monotonic -> clock boottime
  * @offs_tai:			Offset clock monotonic -> clock tai
+ * @offs_aux:			Offset clock monotonic -> clock AUX
  * @coarse_nsec:		The nanoseconds part for coarse time getters
  * @id:				The timekeeper ID
  * @tkr_raw:			The readout base structure for CLOCK_MONOTONIC_RAW
@@ -139,7 +140,10 @@ struct timekeeper {
 	struct timespec64	wall_to_monotonic;
 	ktime_t			offs_real;
 	ktime_t			offs_boot;
-	ktime_t			offs_tai;
+	union {
+		ktime_t		offs_tai;
+		ktime_t		offs_aux;
+	};
 	u32			coarse_nsec;
 	enum timekeeper_ids	id;
 
