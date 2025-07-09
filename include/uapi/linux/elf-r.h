db4be7d4dd891a1f757862024510d6158e68f90a
Thomas Weißschuh <thomas.weissschuh@linutronix.de>
2025-07-09 10:47:26 +0200

    elf, uapi: Add a header for relocation constants
    While the numeric constants for relocations are architecture specific,
    it is still useful to get access to them from other architectures.
    Also the definitions are useful for userspace in general.
    For example tools processing elf files during a kernel cross-compilation
    can make use of them.
    
    Introduce a dedicated header for them, similar to the elf-em.h header.
    For now the header is empty but it will be filled step by step in
    upcomming commits.
    
    Signed-off-by: Thomas Weißschuh <thomas.weissschuh@linutronix.de>

---
+++ include/uapi/linux/elf-r.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
+#ifndef _UAPI_LINUX_ELF_R_H
+#define _UAPI_LINUX_ELF_R_H
+
+/* Relocation constants. Architectures are ordered by their numeric EM_* values */
+
+#endif /* _UAPI_LINUX_ELF_R_H */
