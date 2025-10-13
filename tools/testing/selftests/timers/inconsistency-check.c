e883166a792496a3377852622039f42ed8a840c7
Thomas Weißschuh <thomas.weissschuh@linutronix.de>
2025-10-13 13:02:16 +0200

    selftests: timers: inconsistency-check: Test auxiliary clocks
    Extend the testcase to also cover auxiliary clocks.
    
    Signed-off-by: Thomas Weißschuh <thomas.weissschuh@linutronix.de>

--- tools/testing/selftests/timers/inconsistency-check.c
+++ tools/testing/selftests/timers/inconsistency-check.c
@@ -117,6 +117,14 @@ static const clockid_t all_clocks[] = {
 	CLOCK_REALTIME_ALARM,
 	CLOCK_BOOTTIME_ALARM,
 	CLOCK_TAI,
+	CLOCK_AUX + 0,
+	CLOCK_AUX + 1,
+	CLOCK_AUX + 2,
+	CLOCK_AUX + 3,
+	CLOCK_AUX + 4,
+	CLOCK_AUX + 5,
+	CLOCK_AUX + 6,
+	CLOCK_AUX + 7,
 };
 
 int main(int argc, char *argv[])
