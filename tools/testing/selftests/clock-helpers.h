16ec9a4881b971cabe3d1ca30a249710a791c6a5
Thomas Weißschuh <thomas.weissschuh@linutronix.de>
2025-10-13 11:26:43 +0200

    selftests: clock-helpers: Add auxiliary clocks
    Allow the selftests to also conver auxiliary clocks.
    
    Signed-off-by: Thomas Weißschuh <thomas.weissschuh@linutronix.de>

--- tools/testing/selftests/clock-helpers.h
+++ tools/testing/selftests/clock-helpers.h
@@ -4,6 +4,15 @@
 #define __CLOCK_HELPERS_H
 
 #include <sys/types.h>
+#include <time.h>
+
+#ifndef CLOCK_AUX
+#define CLOCK_AUX 16
+#endif
+
+#ifndef CLOCK_AUX_LAST
+#define CLOCK_AUX_LAST 23
+#endif
 
 __attribute__((unused))
 static inline const char *clock_name(clockid_t clockid)
@@ -31,6 +40,22 @@ static inline const char *clock_name(clockid_t clockid)
 		return "CLOCK_BOOTTIME_ALARM";
 	case CLOCK_TAI:
 		return "CLOCK_TAI";
+	case CLOCK_AUX + 0:
+		return "CLOCK_AUX0";
+	case CLOCK_AUX + 1:
+		return "CLOCK_AUX1";
+	case CLOCK_AUX + 2:
+		return "CLOCK_AUX2";
+	case CLOCK_AUX + 3:
+		return "CLOCK_AUX3";
+	case CLOCK_AUX + 4:
+		return "CLOCK_AUX4";
+	case CLOCK_AUX + 5:
+		return "CLOCK_AUX5";
+	case CLOCK_AUX + 6:
+		return "CLOCK_AUX6";
+	case CLOCK_AUX + 7:
+		return "CLOCK_AUX7";
 	};
 	return "UNKNOWN_CLOCKID";
 }
