0f688f1f93d355a5debc69aaba1eb52b758097c6
Thomas Weißschuh <thomas.weissschuh@linutronix.de>
2025-10-13 11:46:08 +0200

    selftests: vDSO: vdso_test_abi: Use clock_name() from clock-helpers.h
    Replace the custom clockid_t to string translation with the clock_name()
    helper from clock-helpers.h.
    
    The helper does not handle CLOCK_SGI_CYCLE, but the test is not using
    that anyways.
    
    Signed-off-by: Thomas Weißschuh <thomas.weissschuh@linutronix.de>

--- tools/testing/selftests/vDSO/vdso_test_abi.c
+++ tools/testing/selftests/vDSO/vdso_test_abi.c
@@ -19,6 +19,7 @@
 #include <sys/syscall.h>
 
 #include "../kselftest.h"
+#include "../clock-helpers.h"
 #include "vdso_config.h"
 #include "vdso_call.h"
 #include "parse_vdso.h"
@@ -37,21 +38,6 @@ typedef long (*vdso_clock_gettime_t)(clockid_t clk_id, struct timespec *ts);
 typedef long (*vdso_clock_gettime64_t)(clockid_t clk_id, struct vdso_timespec64 *ts);
 typedef long (*vdso_clock_getres_t)(clockid_t clk_id, struct timespec *ts);
 typedef time_t (*vdso_time_t)(time_t *t);
-
-static const char * const vdso_clock_name[] = {
-	[CLOCK_REALTIME]		= "CLOCK_REALTIME",
-	[CLOCK_MONOTONIC]		= "CLOCK_MONOTONIC",
-	[CLOCK_PROCESS_CPUTIME_ID]	= "CLOCK_PROCESS_CPUTIME_ID",
-	[CLOCK_THREAD_CPUTIME_ID]	= "CLOCK_THREAD_CPUTIME_ID",
-	[CLOCK_MONOTONIC_RAW]		= "CLOCK_MONOTONIC_RAW",
-	[CLOCK_REALTIME_COARSE]		= "CLOCK_REALTIME_COARSE",
-	[CLOCK_MONOTONIC_COARSE]	= "CLOCK_MONOTONIC_COARSE",
-	[CLOCK_BOOTTIME]		= "CLOCK_BOOTTIME",
-	[CLOCK_REALTIME_ALARM]		= "CLOCK_REALTIME_ALARM",
-	[CLOCK_BOOTTIME_ALARM]		= "CLOCK_BOOTTIME_ALARM",
-	[10 /* CLOCK_SGI_CYCLE */]	= "CLOCK_SGI_CYCLE",
-	[CLOCK_TAI]			= "CLOCK_TAI",
-};
 
 static void vdso_test_gettimeofday(void)
 {
@@ -86,7 +72,7 @@ static void vdso_test_clock_gettime64(clockid_t clk_id)
 	if (!vdso_clock_gettime64) {
 		ksft_print_msg("Couldn't find %s\n", name[5]);
 		ksft_test_result_skip("%s %s\n", name[5],
-				      vdso_clock_name[clk_id]);
+				      clock_name(clk_id));
 		return;
 	}
 
@@ -97,10 +83,10 @@ static void vdso_test_clock_gettime64(clockid_t clk_id)
 		ksft_print_msg("The time is %lld.%06lld\n",
 			       (long long)ts.tv_sec, (long long)ts.tv_nsec);
 		ksft_test_result_pass("%s %s\n", name[5],
-				      vdso_clock_name[clk_id]);
+				      clock_name(clk_id));
 	} else {
 		ksft_test_result_fail("%s %s\n", name[5],
-				      vdso_clock_name[clk_id]);
+				      clock_name(clk_id));
 	}
 }
 
@@ -113,7 +99,7 @@ static void vdso_test_clock_gettime(clockid_t clk_id)
 	if (!vdso_clock_gettime) {
 		ksft_print_msg("Couldn't find %s\n", name[1]);
 		ksft_test_result_skip("%s %s\n", name[1],
-				      vdso_clock_name[clk_id]);
+				      clock_name(clk_id));
 		return;
 	}
 
@@ -124,10 +110,10 @@ static void vdso_test_clock_gettime(clockid_t clk_id)
 		ksft_print_msg("The time is %lld.%06lld\n",
 			       (long long)ts.tv_sec, (long long)ts.tv_nsec);
 		ksft_test_result_pass("%s %s\n", name[1],
-				      vdso_clock_name[clk_id]);
+				      clock_name(clk_id));
 	} else {
 		ksft_test_result_fail("%s %s\n", name[1],
-				      vdso_clock_name[clk_id]);
+				      clock_name(clk_id));
 	}
 }
 
@@ -165,7 +151,7 @@ static void vdso_test_clock_getres(clockid_t clk_id)
 	if (!vdso_clock_getres) {
 		ksft_print_msg("Couldn't find %s\n", name[3]);
 		ksft_test_result_skip("%s %s\n", name[3],
-				      vdso_clock_name[clk_id]);
+				      clock_name(clk_id));
 		return;
 	}
 
@@ -189,10 +175,10 @@ static void vdso_test_clock_getres(clockid_t clk_id)
 
 	if (clock_getres_fail > 0) {
 		ksft_test_result_fail("%s %s\n", name[3],
-				      vdso_clock_name[clk_id]);
+				      clock_name(clk_id));
 	} else {
 		ksft_test_result_pass("%s %s\n", name[3],
-				      vdso_clock_name[clk_id]);
+				      clock_name(clk_id));
 	}
 }
 
@@ -202,7 +188,7 @@ static void vdso_test_clock_getres(clockid_t clk_id)
  */
 static inline void vdso_test_clock(clockid_t clock_id)
 {
-	ksft_print_msg("clock_id: %s\n", vdso_clock_name[clock_id]);
+	ksft_print_msg("clock_id: %s\n", clock_name(clock_id));
 
 	vdso_test_clock_gettime(clock_id);
 	vdso_test_clock_gettime64(clock_id);
