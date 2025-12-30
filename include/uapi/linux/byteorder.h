f8545522d5319183fb9b08fb389327c11eba494d
Thomas Weißschuh <thomas.weissschuh@linutronix.de>
2025-12-30 08:18:03 +0100

    uapi: Introduce linux/byteorder.h
    Various UAPI headers adapt their definitions to the byteorder of the
    platform. For this detection they make use of asm/byteorder.h.
    
    The direct usage of asm/ headers is discouraged, so add a wrapper.
    
    Signed-off-by: Thomas Weißschuh <thomas.weissschuh@linutronix.de>

---
+++ include/uapi/linux/byteorder.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
+#ifndef _UAPI_LINUX_BYTEORDER_H
+#define _UAPI_LINUX_BYTEORDER_H
+
+#include <asm/byteorder.h>
+
+#endif /* _UAPI_LINUX_BYTEORDER_H */
