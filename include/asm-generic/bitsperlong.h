706abbfcc19056ba08b8c29bf693cc2f6924acd4
Thomas Weißschuh <thomas.weissschuh@linutronix.de>
2026-01-14 14:55:52 +0100

    BUILD_VDSO

--- include/asm-generic/bitsperlong.h
+++ include/asm-generic/bitsperlong.h
@@ -11,12 +11,10 @@
 #define BITS_PER_LONG 32
 #endif /* CONFIG_64BIT */
 
-/*
- * FIXME: The check currently breaks x86-64 build, so it's
- * temporarily disabled. Please fix x86-64 and reenable
- */
-#if 0 && BITS_PER_LONG != __BITS_PER_LONG
+#ifndef BUILD_VDSO
+#if BITS_PER_LONG != __BITS_PER_LONG
 #error Inconsistent word size. Check asm/bitsperlong.h
+#endif
 #endif
 
 #define BITS_PER_LONG_LONG 64
