7e301e86c1b7f1b358b04d948bef9d2e888899c4
Thomas Weißschuh <thomas.weissschuh@linutronix.de>
2026-01-14 11:42:55 +0100

    asm-generic/bitsperlong.h: Add sanity checks for __BITS_PER_LONG
    The value of __BITS_PER_LONG from architecture-specific logic should
    always match the generic one if that is available. It should also match
    the actual C type 'long'.
    
    Mismatches can happen for example when building the compat vDSO. Either
    during the compilation, see commit 9a6d3ff10f7f ("arm64: uapi: Provide
    correct __BITS_PER_LONG for the compat vDSO"), or when running sparse
    when mismatched CHECKFLAGS are inherited from the kernel build.
    
    Add some consistency checks which detect such issues early and clearly.
    
    The kernel-internal BITS_PER_LONG is not checked as it is derived from
    CONFIG_64BIT and therefore breaks for the compat vDSO. See the similar,
    deactivated check above.
    
    Signed-off-by: Thomas Weißschuh <thomas.weissschuh@linutronix.de>
    Acked-by: Arnd Bergmann <arnd@arndb.de>

--- include/asm-generic/bitsperlong.h
+++ include/asm-generic/bitsperlong.h
@@ -19,6 +19,15 @@
 #error Inconsistent word size. Check asm/bitsperlong.h
 #endif
 
+#if __BITS_PER_LONG != __CHAR_BIT__ * __SIZEOF_LONG__
+#error Inconsistent word size. Check asm/bitsperlong.h
+#endif
+
+#ifndef __ASSEMBLER__
+_Static_assert(sizeof(long) * 8 == __BITS_PER_LONG,
+	       "Inconsistent word size. Check asm/bitsperlong.h");
+#endif
+
 #ifndef BITS_PER_LONG_LONG
 #define BITS_PER_LONG_LONG 64
 #endif
