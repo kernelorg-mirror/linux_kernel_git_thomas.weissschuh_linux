0a7ee485fdfcaeccf189419833ac0c9b330df969
Thomas Weißschuh <linux@weissschuh.net>
2026-01-25 12:06:32 +0100

    hid-bus-match

--- drivers/hid/hid-core.c
+++ drivers/hid/hid-core.c
@@ -2671,7 +2671,7 @@ EXPORT_SYMBOL_GPL(hid_match_device);
 
 static int hid_bus_match(struct device *dev, const struct device_driver *drv)
 {
-	struct hid_driver *hdrv = to_hid_driver(drv);
+	const struct hid_driver *hdrv = to_hid_driver(drv);
 	struct hid_device *hdev = to_hid_device(dev);
 
 	return hid_match_device(hdev, hdrv) != NULL;
