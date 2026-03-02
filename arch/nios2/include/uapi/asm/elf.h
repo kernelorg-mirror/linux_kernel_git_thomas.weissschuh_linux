75bcd5e6c4b2787b54ed47b0976dca4d7c13d9a6
Thomas Weißschuh <thomas.weissschuh@linutronix.de>
2026-03-02 15:57:01 +0100

    nios2: Move relocation constants to the UAPI
    These constants are useful for cross-platform userspace, for example to
    process ELF files during kernel cross-compilation.
    
    Move them from the kernel-private architecture-specific header to the UAPI.

--- arch/nios2/include/uapi/asm/elf.h
+++ arch/nios2/include/uapi/asm/elf.h
@@ -23,30 +23,6 @@
 
 #include <linux/ptrace.h>
 
-/* Relocation types */
-#define R_NIOS2_NONE		0
-#define R_NIOS2_S16		1
-#define R_NIOS2_U16		2
-#define R_NIOS2_PCREL16		3
-#define R_NIOS2_CALL26		4
-#define R_NIOS2_IMM5		5
-#define R_NIOS2_CACHE_OPX	6
-#define R_NIOS2_IMM6		7
-#define R_NIOS2_IMM8		8
-#define R_NIOS2_HI16		9
-#define R_NIOS2_LO16		10
-#define R_NIOS2_HIADJ16		11
-#define R_NIOS2_BFD_RELOC_32	12
-#define R_NIOS2_BFD_RELOC_16	13
-#define R_NIOS2_BFD_RELOC_8	14
-#define R_NIOS2_GPREL		15
-#define R_NIOS2_GNU_VTINHERIT	16
-#define R_NIOS2_GNU_VTENTRY	17
-#define R_NIOS2_UJMP		18
-#define R_NIOS2_CJMP		19
-#define R_NIOS2_CALLR		20
-#define R_NIOS2_ALIGN		21
-
 typedef unsigned long elf_greg_t;
 
 #define ELF_NGREG		49
