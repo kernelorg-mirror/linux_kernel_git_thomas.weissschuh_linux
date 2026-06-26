33f8c7d73eac363531bb199c879bbea064dbc25e
Thomas Weißschuh (Schneider Electric) <thomas.weissschuh@linutronix.de>
2026-06-26 15:02:31 +0200

    timekeeping: Prepare for additional sequence counts in tk_clock_offsets
    The implementation of hrtimers for auxiliary clocks will require an
    additional sequence counter in this structure. To prepare for that while
    keeping the modification check fast introduce a union which will hold
    both sequence counters.
    
    Signed-off-by: Thomas Weißschuh (Schneider Electric) <thomas.weissschuh@linutronix.de>

--- include/linux/timekeeping_types.h
+++ include/linux/timekeeping_types.h
@@ -28,12 +28,18 @@ struct tk_aux_mono_conv {
 /**
  * struct tk_clock_offsets - clock offsets to CLOCK_MONOTONIC
  * @clock_was_set_seq:	Sequence counter for clock updates.
+ * @clock_seq:		Combined sequence counter.
  * @offs_real:		Offset to CLOCK_REALTIME.
  * @offs_boot:		Offset to CLOCK_BOOTTIME.
  * @offs_tai:		Offset to CLOCK_TAI.
  */
 struct tk_clock_offsets {
-	u32			clock_was_set_seq;
+	union {
+		struct {
+			u32	clock_was_set_seq;
+		};
+		u64		clock_seq;
+	};
 	ktime_t			offs_real;
 	ktime_t			offs_boot;
 	ktime_t			offs_tai;
--- kernel/time/hrtimer.c
+++ kernel/time/hrtimer.c
@@ -901,7 +901,7 @@ static bool update_needs_ipi(struct hrtimer_cpu_base *cpu_base, unsigned int act
 {
 	struct hrtimer_clock_base *base;
 	ktime_t expires;
-	u32 seq;
+	u64 seq;
 
 	/*
 	 * Update the base offsets unconditionally so the following
@@ -913,14 +913,14 @@ static bool update_needs_ipi(struct hrtimer_cpu_base *cpu_base, unsigned int act
 	 * it will see it when it finishes the processing and reevaluates
 	 * the next expiring timer.
 	 */
-	seq = cpu_base->tk_offsets.clock_was_set_seq;
+	seq = cpu_base->tk_offsets.clock_seq;
 	hrtimer_update_base(cpu_base);
 
 	/*
 	 * If the sequence did not change over the update then the
 	 * remote CPU already handled it.
 	 */
-	if (seq == cpu_base->tk_offsets.clock_was_set_seq)
+	if (seq == cpu_base->tk_offsets.clock_seq)
 		return false;
 
 	/* If a deferred rearm is pending the remote CPU will take care of it */
