75b4b087627360f19af20028aaba9a9e1b8b2997
Thomas Weißschuh <linux@weissschuh.net>
2026-07-14 18:45:07 +0200

    pci2

--- drivers/pci/endpoint/functions/pci-epf-vntb.c
+++ drivers/pci/endpoint/functions/pci-epf-vntb.c
@@ -1239,7 +1239,7 @@ CONFIGFS_ATTR(epf_ntb_, mw2_bar);
 CONFIGFS_ATTR(epf_ntb_, mw3_bar);
 CONFIGFS_ATTR(epf_ntb_, mw4_bar);
 
-static struct configfs_attribute *epf_ntb_attrs[] = {
+static const struct configfs_attribute *const epf_ntb_attrs[] = {
 	&epf_ntb_attr_spad_count,
 	&epf_ntb_attr_db_count,
 	&epf_ntb_attr_num_mws,
@@ -1260,7 +1260,7 @@ static struct configfs_attribute *epf_ntb_attrs[] = {
 };
 
 static const struct config_item_type ntb_group_type = {
-	.ct_attrs	= epf_ntb_attrs,
+	.ct_attrs_const	= epf_ntb_attrs,
 	.ct_owner	= THIS_MODULE,
 };
 
